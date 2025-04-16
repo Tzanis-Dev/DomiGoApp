1dc9e087e24161a89e518616cbc45ed48b7f084f
Tzanis-Dev <ioannis.tzanakis@gmail.com>
2025-04-16 14:13:01 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to DomiGoApp</h1>
+      <p>This is a construction workflow app to measure and optimize jobs and processes.</p>
+    </div>
+  );
+}
+
+export default App;
